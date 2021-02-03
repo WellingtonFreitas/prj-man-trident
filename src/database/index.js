31772c502cdafd45a3d2014299dec0ab867ad729
WellingtonFreitas <w.martins.de.freitas@avanade.com>
2021-02-03 08:03:14 -0300

    Mudanca de banco local para banco em cloud

--- src/database/index.js
+++ src/database/index.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-
     try {
-        mongoose.connect('mongodb://localhost/project_managementeDB', { useNewUrlParser: true, 
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true });
-        console.log('Conecting databese sucessful');
+        // mongoose.connect('mongodb://localhost/project_managementeDB', { useNewUrlParser: true, 
+        mongoose.connect('mongodb+srv://wellington:Wl@02062017@trident-db.8vqxp.mongodb.net/trident-db?retryWrites=true&w=majority', {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true });
+            console.log('Conecting databese sucessful');
     } catch (err) {
         console.log('Conecting database error. '+err);
     };  
